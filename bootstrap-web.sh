e9b15edc6368453c881c8e6fa7f42fa6587304fe
Christopher Nelson <nadiasvertex@gmail.com>
2015-04-08 22:32:21 -0400

    Fixed the uwsgi startup to use daemonize and to log data.

--- bootstrap-web.sh
+++ bootstrap-web.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y ntp
 
 # Get pypy
 wget -c -nv https://bitbucket.org/pypy/pypy/downloads/pypy-2.5.1-linux64.tar.bz2
-tar -xf pypy-2.5.1-linux64.tar.bz2 #&& rm pypy-2.5.1-linux64.tar.bz2
+tar -xf pypy-2.5.1-linux64.tar.bz2
 
 # Get pip
 wget -c -nv https://bootstrap.pypa.io/get-pip.py
@@ -22,10 +22,12 @@ find /vagrant/ -name "*.py" | xargs -iTARG ln -s TARG ${APP}/
 
 cat > rc.local <<rc_local_data
 #!/bin/bash
-cd ${APP} && ${UWSGI} --http :9090 --pypy-wsgi app --master --processes 2 --stats :9191
+cd ${APP} && ${UWSGI} --http :80 --pypy-wsgi app --master --processes 2 --stats :9191 --daemonize /var/log/app.log
 rc_local_data
 chmod +x rc.local
 sudo cp rc.local /etc/rc.local
+
+sudo bash rc.local
 
 sudo cp /vagrant/ntp-slave.conf /etc/ntp.conf
 sudo service ntp restart
