5f59723d5b334a0284bf1a1677d2366f479b72eb
Christopher Nelson <nadiasvertex@gmail.com>
2015-04-09 23:42:15 -0400

    Updated perf test and max files.

--- app/test/perf.sh
+++ app/test/perf.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-httperf --hog --uri=http://localhost:8080/bookmark/uid:10001/cl:E --num-conn 1000 --ra 100
+httperf --server=localhost --port=8080 --uri=http://localhost:8080/bookmark/uid:10001/cl:E --num-conn=10000 --ra=1000
