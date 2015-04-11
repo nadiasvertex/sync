95fbd9b12bd1f21420d566aa8c26c13b2291a596
Christopher Nelson <nadiasvertex@gmail.com>
2015-04-11 18:57:13 -0400

    Merged from zookeeper.

--- update-web.sh
+++ update-web.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for WEB in web1 web2 web3; do
-    vagrant ssh ${WEB} -c "rsync -av --delete --exclude \"*.pyc\" --exclude __pycache__ /vagrant/app /home/vagrant/app; sudo service sync restart"
+    vagrant ssh ${WEB} -c "rsync -av --delete --exclude \"*.pyc\" --exclude __pycache__ /vagrant/app/ /home/vagrant/app/; sudo service sync restart"
 done
